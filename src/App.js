3584ca831928b5849a9afe44a444da17b81cdf1a
duxuanrain <598991476qq.com>
2017-10-22 01:20:54 +0800

    add react

---
+++ src/App.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+import './App.css';
+import TodoInput from './TodoInput';
+import 'normalize.css';
+import './reset.css';
+import TodoItem from './TodoItem'
+import './TodoItem.css'
+import Userdialog from './Userdialog'
+import {getCurrentUser,signOut} from './leanCloud'
+
+
+
+
+
+class App extends Component {
+  constructor(props){
+    super(props)
+      this.state = {
+        user:getCurrentUser() || {},
+      newTodo:'',
+          todoList:[]
+      }
+  }
+  render() {
+    let todos = this.state.todoList.filter((item)=>!item.deleted).map((item,index)=>{
+      return (
+          <li key={index}>
+          <TodoItem todo={item} onToggle={this.toggle.bind(this)} onDelete={this.delete.bind(this)}/>
+          </li>
+          )
+        })
+    return (
+      <div className="App">
+       <h1>{this.state.user.usernamr||'我'}的待办
+           {this.state.user.id? <button onClick={this.signOut.bind(this)}>登出</button>:null}
+      </h1>
+              <div className = "inputWrapper">
+              <TodoInput content={this.state.newTodo} onChange={this.changeTitle.bind(this)}
+              onSubmit={this.addTodo.bind(this)}/>
+              </div>
+          <ol className="todoList">
+             {todos}
+          </ol>
+          {this.state.user.id?null:<Userdialog onSignUp={this.onSignUpOrSignIn.bind(this)} onSignIn={this.onSignUpOrSignIn.bind(this)}/>}
+      </div>
+  )
+  }
+  signOut(){
+      signOut()
+      let stateCopy = JSON.parse(JSON.stringify(this.state))
+      this.state.user = {}
+      this.setState(stateCopy)
+  }
+  onSignUpOrSignIn(user){
+      let stateCopy = JSON.parse(JSON.stringify(this.state))
+      this.state.user = user
+      this.setState(stateCopy)
+  }
+  componentDidUpdate(){}
+  toggle(e,todo){
+      todo.status=todo.status = 'completed'?'':'completed'
+    this.setState(this.state)
+
+}
+  changeTitle(event){
+      this.setState({
+          newTodo:event.target.value,
+          todoList:this.state.todoList
+      })
+
+  }
+  addTodo(event){
+      this.state.todoList.push({
+          id:idMaker(),
+          title:event.target.value,
+          status:null,
+          default:false
+      })
+      this.setState({
+          newTodo:'',
+          todoList:this.state.todoList
+      })
+
+  }
+  delete(event,todo){
+      todo.deleted = true
+      this.setState(this.state)
+
+  }
+}
+
+export default App;
+
+let id = 0
+function idMaker() {
+    id += 1;
+    return id
+}
